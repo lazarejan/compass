52dbe68f63f49251dce6d57c285fc60150da140c
lazare janiashvili <lazarejaniashvili9@gmail.com>
2025-02-08 15:52:21 +0400

    edited last bug and consumer exp, debuged

--- cmpss.py
+++ cmpss.py
@@ -1,12 +1,16 @@
 def calculation(deg):
     if deg > 360:
         deg = deg % 360
-    dirs = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"]
-    upd_deg = deg // 45
-    if deg % 45 >= 20:
-        targ = dirs[upd_deg + 1] + "°"
+    dirs = ("N", "NE", "E", "SE", "S", "SW", "W", "NW", "N")
+    upd_deg = deg // 90
+    real_deg = deg % 90
+
+    if real_deg < 20:
+        targ = dirs[upd_deg + upd_deg]
+    elif real_deg < 70:
+        targ = dirs[upd_deg + 1+ upd_deg]
     else:
-        targ = dirs[upd_deg] + "°"
+        targ = dirs[upd_deg + 2+ upd_deg]
     return targ
 
 def comp():
@@ -18,3 +22,7 @@ def comp():
         else:
             break
     return calculation(deg)
+
+def consumer_ret():
+    return f"you are looking at the direction of {comp()}°"
+print(consumer_ret())
